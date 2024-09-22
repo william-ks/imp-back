4236fab2a3e30096b5c12bf0d2f39036dd2f2929
William Krisley <will.ks2101@gmail.com>
2024-09-21 23:04:32 -0300

    feat: admin model

---
+++ src/api/modules/Admin/model/Admin.ts
@@ -0,0 +1,25 @@
+import { nanoid } from "nanoid";
+
+export class Admin {
+	public readonly id?: number;
+	public readonly publicId: string;
+	public name: string;
+	public email: string;
+	public password: string;
+
+	public isActive: boolean = false;
+	public isDisabled: boolean = false;
+
+	public createdAt: Date = new Date();
+	public updatedAt: Date;
+
+	constructor(props: Omit<Admin, "publicId">, publicId?: string) {
+		Object.assign(this, props);
+
+		if (!publicId) {
+			this.publicId = nanoid();
+		} else {
+			this.publicId = publicId;
+		}
+	}
+}
